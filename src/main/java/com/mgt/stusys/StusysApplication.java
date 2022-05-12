b78829edbc1b4b9d9a35de4e1c04f5cf45f6e125
SahanDiaz <sahandiaz1@gmail.com>
2022-05-12 23:55:13 +0530

    student mgt sys - test

---
+++ src/main/java/com/mgt/stusys/StusysApplication.java
@@ -0,0 +1,13 @@
+package com.mgt.stusys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StusysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StusysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mgt/stusys/StusysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgt.stusys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StusysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
